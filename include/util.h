28a246a5925033990e093bcee4250f41a8888bb0
Geovani de Souza <geovanisouza92@gmail.com>
2012-07-29 14:01:47 -0300

    Build funcionando, porém falta resolver o problema de pré-geração dos arquivos parser.cpp e scanner.cpp

--- include/util.h
+++ include/util.h
@@ -13,11 +13,11 @@ using namespace std;
 namespace LANG_NAMESPACE {
     namespace Util {
         const clock_t start(clock());
-        static string intToStr(int);
-        static string formatNumber(int, int, char);
-        static double getMili();
-        static string statistics(unsigned, unsigned, bool);
-        static string trimString(string, char);
+        string intToStr(int);
+        string formatNumber(int, int, char);
+        double getMili();
+        string statistics(unsigned, unsigned, bool);
+        string trimString(string, char);
     }
 }
 
