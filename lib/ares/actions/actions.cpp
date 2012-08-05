4863c1adaf82b707c06ade535b6c7d2be9edd0b7
Geovani de Souza <geovanisouza92@gmail.com>
2012-08-05 19:22:05 -0300

    Adicionando arquivo de produções da gramática

---
+++ lib/ares/actions/actions.cpp
@@ -0,0 +1,18 @@
+/* Ares Programming Language */
+
+#ifndef PARSER_ACTIONS
+#define PARSER_ACTIONS
+
+#include <iostream>
+
+using namespace std;
+
+#line 175 "parser.y"
+#define Goal_empty \
+	cout << "Vazio..." << endl;
+
+#line 176 "parser.y"
+#define Goal_GoalRepeat \
+	cout << "Deu certo!" << endl;
+
+#endif
