8197e9a3a1d14f559721d071c588402503f21192
Geovani de Souza <geovanisouza92@gmail.com>
2012-11-28 20:17:06 -0200

    Adaptações para integração com Python

--- tools/arc/main.cpp
+++ tools/arc/main.cpp
@@ -55,113 +55,119 @@ namespace programOptions = boost::program_options;
 #ifdef LANG_DEBUG
 #define STATS(x) \
 if (driver.verboseMode >= VerboseMode::Low) { \
-    string errors = driver.resumeMessages (); \
-    if (!errors.empty ()) \
+    string errors = driver.resumeMessages(); \
+    if (!errors.empty()) \
         output << "=> " << errors; \
     if (driver.totalLines > 0) \
-        output << statistics (filesProcessed, driver.totalLines, x); \
+        output << statistics(filesProcessed, driver.totalLines, x); \
     if (driver.verboseMode >= VerboseMode::High) { \
-        output << "=> Elapsed miliseconds: " << getMili () << endl; \
+        output << "=> Elapsed miliseconds: " << getMili() << endl; \
     } \
 }
 #else
 #define STATS(x) \
 if (driver.verboseMode >= VerboseMode::Low) { \
-    string errors = driver.resumeMessages (); \
-    if (!errors.empty ()) \
+    string errors = driver.resumeMessages(); \
+    if (!errors.empty()) \
         output << "=> " << errors; \
     if (driver.totalLines > 0) \
-        output << statistics (filesProcessed, driver.totalLines, x); \
+        output << statistics(filesProcessed, driver.totalLines, x); \
 }
 #endif
 #endif
 
 int
-main (int argc, char ** argv)
+main(int argc, char ** argv)
 {
     unsigned maxErrors = 3, filesProcessed = 0;
     InteractionMode::Mode mode = InteractionMode::None;
     vector<string> files, messages;
-    string outputFilename (LANG_SHELL_NAME ".out"), line;
+    string outputFilename(LANG_SHELL_NAME ".out"), line;
     std::ostream & output = std::cout;
+    bool echo = true;
     VirtualEngine::Driver driver(output);
 
-    programOptions::options_description desc ("Usage: " LANG_SHELL_NAME " [Options] files\n\nOptions");
-    desc.add_options ()
+    programOptions::options_description desc("Usage: " LANG_SHELL_NAME " [Options] files\n\nOptions");
+    desc.add_options()
         ("check-only,c", "Enable check only")
-        ("eval,e", programOptions::value<string> (), "Evaluate and compile <arg>")
+        ("eval,e", programOptions::value<string>(), "Evaluate and compile <arg>")
         ("help,h", "Print this message and exit")
-        ("input-file", programOptions::value<vector<string> > (), "[Optional] Use <arg> as input file(s).")
-        ("verbose", programOptions::value<int> ()->default_value (0), "Set the verbose mode [0..3]")
-        ("version,v", "Print version information");
+        ("input-file,i", programOptions::value<vector<string> >(), "[Optional] Use <arg> as input file(s).")
+        // ("output-file,o", programOptions::value<vector<string> >(), "[Optional] Set <arg> as output file(s).")
+        ("verbose,x", programOptions::value<int>()->default_value(0), "Set the verbose mode [0..3]")
+        ("version,v", "Print version information")
+        ("echo,z", programOptions::value<bool>()->default_value(true), "Set echo for console");
     programOptions::positional_options_description positional_input;
-    positional_input.add ("input-file", -1);
+    positional_input.add("input-file", -1);
 
     programOptions::variables_map options;
     try {
-        programOptions::store (programOptions::command_line_parser (argc, argv)
-            .options (desc).positional (positional_input).run (), options);
-        notify (options);
-    } catch (programOptions::unknown_option & e) {
-        output << err_tail "Unknown option: " << e.get_option_name () << endl;
+        programOptions::store(programOptions::command_line_parser(argc, argv)
+            .options(desc).positional(positional_input).run(), options);
+        notify(options);
+    } catch(programOptions::unknown_option & e) {
+        output << err_tail "Unknown option: " << e.get_option_name() << endl;
         output << desc << endl;
         return 1;
     }
 
-    if (options.count ("check-only")) {
+    if (options.count("check-only")) {
         driver.checkOnly = true;
     }
 
-    if (options.count ("eval")) {
+    if (options.count("eval")) {
         mode = InteractionMode::LineEval;
-        line = options ["eval"].as<string> ();
-        if (!line.empty ()) {
-            bool parse_result = driver.parseString (line, "line eval");
+        line = options ["eval"].as<string>();
+        if (!line.empty()) {
+            bool parse_result = driver.parseString(line, "line eval");
             if (parse_result) {
                 if (driver.errors > maxErrors) return 1;
-                driver.produce ( (driver.checkOnly ? FinallyAction::None : FinallyAction::PrintOnConsole), output);
+                driver.produce((driver.checkOnly ? FinallyAction::None : FinallyAction::PrintOnConsole), output);
             }
             STATS(false)
         }
     }
 
-    if (options.count ("help")) {
+    if (options.count("help")) {
         output << desc << endl;
         return 0;
     }
 
-    if (options.count ("input-file")) {
-        vector<string> input_files = options ["input-file"].as<vector<string> > ();
-        for (vector<string>::iterator file = input_files.begin (); file < input_files.end (); file++) {
-            files.push_back (*file);
+    if (options.count("input-file")) {
+        vector<string> input_files = options ["input-file"].as<vector<string> >();
+        for (vector<string>::iterator file = input_files.begin(); file < input_files.end(); file++) {
+            files.push_back(*file);
         }
     }
 
-    if (options.count ("verbose")) {
-        driver.verboseMode = (VerboseMode::Mode) options ["verbose"].as<int> ();
+    if (options.count("verbose")) {
+        driver.verboseMode =(VerboseMode::Mode) options ["verbose"].as<int>();
     }
 
-    if (options.count ("version")) {
-        output << langVersionInfo () << endl;
+    if (options.count("version")) {
+        output << langVersionInfo() << endl;
         return 0;
     }
 
+    if (options.count("echo"))
+        echo = options ["echo"].as<bool>();
+
     if (driver.verboseMode >= VerboseMode::High) {
-        output << langVersionInfo () << endl;
-        if (!messages.empty ())
-            for (vector<string>::iterator message = messages.begin (); message < messages.end (); message++)
+        output << langVersionInfo() << endl;
+        if (!messages.empty())
+            for (vector<string>::iterator message = messages.begin(); message < messages.end(); message++)
                 output << *message << endl;
     }
 
     // TODO Transpor código para Driver
     if (!files.empty()) {
         mode = InteractionMode::FileParse;
-        for (vector<string>::iterator file = files.begin (); file < files.end (); file++) {
-            bool parse_ok = driver.parseFile (*file);
+        for (vector<string>::iterator file = files.begin(); file < files.end(); file++) {
+            bool parse_ok = driver.parseFile(*file);
             if (parse_ok) {
                 filesProcessed++;
                 if (driver.errors > maxErrors) return 1;
-                driver.produce ( (driver.checkOnly ? FinallyAction::None : FinallyAction::PrintOnConsole), output);
+                driver.produce((driver.checkOnly ? FinallyAction::None : FinallyAction::PrintOnConsole), output);
             } else
                 break;
         }
@@ -169,16 +175,19 @@ main (int argc, char ** argv)
     } else if (mode == InteractionMode::None) mode = InteractionMode::Shell;
 
     if (mode == InteractionMode::Shell) {
-        string guide (">>> ");
+        string guide(">>> ");
         int blanks = 0;
-        while (output << COLOR_BGREEN<< LANG_SHELL_NAME << COLOR_RESET << guide && getline (cin, line))
-            if (!line.empty ()) {
-                bool parse_ok = driver.parseString (line, LANG_SHELL_NAME);
+        string echo_shell = "";
+        if (echo)
+            echo_shell = COLOR_BGREEN LANG_SHELL_NAME COLOR_RESET + guide;
+        while(output << echo_shell && getline(cin, line))
+            if (!line.empty()) {
+                bool parse_ok = driver.parseString(line, LANG_SHELL_NAME);
                 if (parse_ok) {
                     if (driver.errors > maxErrors) break;
-                    driver.produce ( (driver.checkOnly ? FinallyAction::None : FinallyAction::PrintOnConsole), output);
+                    driver.produce((driver.checkOnly ? FinallyAction::None : FinallyAction::PrintOnConsole), output);
                 }
-                blanks = 0; line.clear ();
+                blanks = 0; line.clear();
             } else if (++blanks && blanks >= 3) break;
         STATS(false)
     }
